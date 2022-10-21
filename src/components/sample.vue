fb36d7c0e0667701959946934af83fec3dcf085a
caicodes <caicodes1@gmail.com>
2022-10-21 15:40:40 -0600

    styled list and input basics

--- src/components/sample.vue
+++ src/components/sample.vue
@@ -12,9 +12,9 @@ const dataObj = {
 
 const runFilter = () => {
   console.log('runFilter called')
-
-  Object.assign(resultListFiltered, dataObj)
-
+  for (const [key, value] of Object.entries(dataObj)) {
+    console.log(`${key}: ${value}`)
+  }
 }
 const resultListFiltered = computed(() => {
   return runFilter
@@ -22,14 +22,31 @@ const resultListFiltered = computed(() => {
 </script>
 
 <template>
-  <input type="text" v-model="inputField" @input="runFilter">
-  <ul>
-    <li><span>a</span> is 1</li>
-  </ul>
+  <div>
+    <input type="text" v-model="inputField" @input="runFilter">
+    <ul>
+      <li><span>a</span> is 1</li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+div {
+  margin-top: 10vh;
+}
+
+input {
+  padding: .5rem;
+}
+
 ul {
   list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    text-align: start;
+    padding: .5rem;
+  }
 }
 </style>
